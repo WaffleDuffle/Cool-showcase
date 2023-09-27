bb7369837e1d434a116e3fb910df65c78cdcfba1
Constantin Denis <denis.io.constantin@gmail.com>
2023-09-27 21:13:53 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+//check OS
 #ifdef _WIN32
     #include <Windows.h>
     void foo(){
@@ -16,7 +17,7 @@
 #include <vector>
 #include <string>
 #include <cstring>
-
+ //convert sentence into upper
 void big_letters(char *string){
     for(int i = 0 ; i < strlen(string) ; ++i){
         if(string[i] >= 'a' && string[i] <= 'z')
@@ -25,6 +26,7 @@ void big_letters(char *string){
 }
 
 int main(){
+    //create escape sequences vector
     std::vector<std::string> colour{"\e[1;30m","\e[1;31m","\e[1;32m","\e[1;33m","\e[1;34m","\e[1;35m","\e[1;36m","\e[1;37m"
     ,"\e[1;90m","\e[1;91m"};
     int random_param;
@@ -35,7 +37,7 @@ int main(){
     while(true){
         for(int i = 0 ; i < 20 ; ++i, std::cout << "\n"){
             for(int j = 0 ; j < colour.size() ; ++j){
-                random_param = rand()%10;
+                random_param = rand()%10; // magic
                 std::cout << colour[random_param] << character << ' ';     
             }
         }
